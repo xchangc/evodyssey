39025cfc83040d19b58bd49fb51d391dc19b04f3
259907 <259907@AI911LAB235P27>
2017-03-06 15:37:20 -0800

    GP
    -Worked on base scripts for entities
    -Added LivingEntities
    -Added Ability
    -Added LivingPlayer
    -Added Mutation
    -created a class for enums called EvoEnums

---
+++ evodyssey/Assets/Scripts/EvoEnums.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvoEnums : MonoBehaviour
+{
+
+    public enum AbilityType
+    {
+        Attack,
+        Defense,
+        Escape
+    }
+
+
+}
---
+++ evodyssey/Assets/Scripts/LivingEntities/Ability.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ability : MonoBehaviour
+{
+
+    #region Variables
+
+    string mName;
+    public EvoEnums.AbilityType mType;
+    float mValue;
+
+    #endregion
+
+    #region Functions
+
+    public virtual void Init() { }
+    public virtual void Run() { }
+    public virtual void Exit() { }
+
+    public float GetValue() { return mValue; }
+    public string GetName() { return mName; }
+
+    #endregion
+}
